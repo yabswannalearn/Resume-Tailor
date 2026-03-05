565a54f5788c23eb9b427dc7ea15a90db2c198e2
reinael123 <reinaelyabut1234@gmail.com>
2026-03-05 11:56:36 +0800

    feat: route for resume builder

--- app/routers/resume.py
+++ app/routers/resume.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from modules import job_analyzer, identity_loader
+from modules import job_analyzer, identity_loader, resume_builder
 
 router = APIRouter(prefix="/resume", tags=["Resume"])
 
@@ -14,8 +14,15 @@ def analyze_job(body: JobInput):
 
 
 @router.post("/generate")
-def generate_resume():
-    return {"message": "generate endpoint is working!"}
+def generate_resume(body: JobInput):
+    #analyze
+    job_data = job_analyzer.analyze(body.job_input)
+    #load identity
+    identity_data = identity_loader.load()
+    #build the resume
+    tailored_resume = resume_builder.build(job_data, identity_data)
+
+    return tailored_resume
 
 @router.get("/load-identity")
 def load_identity():
